afcf561187eb3cb75d7d2a76ebc2e8fa33071dd9
choudhariprathamesh96-eng <choudhariprathamesh96@gmail.com>
2026-08-26 10:18:29 +0530

    Update WebConfig.java

--- WebConfig.java
+++ WebConfig.java
@@ -1,11 +1,32 @@
 package com.propertyhub.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://property-hub-system2.netlify.app",
+                        "http://localhost:5500",
+                        "http://127.0.0.1:5500"
+                )
+                .allowedMethods(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "OPTIONS"
+                )
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
